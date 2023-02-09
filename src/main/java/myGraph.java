febde419ed30a6e4a5370b181edc6566668db47e
yingura <gina_brasen@cox.net>
2023-02-08 17:28:20 -0700

    Feature 1

---
+++ src/main/java/myGraph.java
@@ -0,0 +1,60 @@
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.nio.dot.DOTExporter;
+import org.jgrapht.nio.dot.DOTImporter;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class myGraph {
+    static Graph<String, DefaultEdge> graph;
+    static String nodeCount;
+    static String nodeLabels;
+    static String edgeCount;
+    static String direction;
+
+    myGraph(Graph<String, DefaultEdge> graph){
+        this.graph = graph;
+        this.nodeCount = graph.vertexSet().size() + "";
+        this.nodeLabels = graph.vertexSet() + "";
+        this.edgeCount = graph.edgeSet().size() + "";
+        this.direction = (graph.edgeSet() + "").replace(":", "->");
+    }
+
+    @Override
+    public String toString() {
+        return ("Number of nodes: " + nodeCount + "\n" +
+                "Label of the nodes: " + nodeLabels + "\n" +
+                "Number of edges: " + edgeCount + "\n" +
+                "Nodes and edge directions: " + direction);
+    }
+
+    public static void main(String[] args) throws IOException {
+        parseGraph("C:\\Users\\sakur\\Downloads\\test.txt");
+
+        System.out.println(new myGraph(graph));
+
+        outputGraph("C:\\Users\\sakur\\Downloads\\output.txt");
+    }
+
+    public static void parseGraph(String filepath) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filepath)));
+
+        graph = new SimpleDirectedGraph<>(DefaultEdge.class);
+        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>();
+        importer.setVertexFactory(id->id);
+        importer.importGraph(graph, new StringReader(content));
+    }
+
+    public static void outputGraph(String filepath) throws IOException {
+        DOTExporter<String, DefaultEdge> exporter = new DOTExporter<>();
+        Writer writer = new StringWriter();
+        exporter.exportGraph(graph, writer);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filepath));
+        bufferedWriter.write(writer.toString());
+        bufferedWriter.close();
+    }
+
+}
